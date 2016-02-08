13d26fe1d73e866b15ee7fed24d5df273fdce469
TonyC1962 <ajcgoldrush@gmail.com>
2016-02-08 14:00:10 -0500

    initial commit

---
+++ scripts/app.js
@@ -0,0 +1,31 @@
+"use strict";
+
+// set up the IIFE (Pronounced 'IFFY')(Immediately Invoked Function Expression)
+(function(){
+
+	console.log("App Started...");
+//anything inside the braces is called "Code Block" 
+
+// declared a named function way
+	/*function replaceFirstParagraph() {
+		console.log("inside replaceFirstParagraph function");
+		var firstParagraph;
+
+		firstParagraph = document.getElementById("firstParagraph");
+
+		firstParagraph.innerHTML = "My New Paragraph Data";
+}
+*/
+// declare anonymous function with named alias
+	var replaceFirstParagraph = function() {
+	console.log("inside replaceFirstParagraph anonymous function");
+		var firstParagraph;
+
+		firstParagraph = document.getElementById("firstParagraph");
+
+		firstParagraph.innerHTML = "Welcome to my portfolio!";
+};
+// call the replaceFirstParagraph function
+	replaceFirstParagraph();
+
+})();
