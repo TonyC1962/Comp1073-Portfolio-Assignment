5c3eed0bd3476238bb04b7f7b5b98d7bb9347ac4
TonyC1962 <ajcgoldrush@gmail.com>
2016-02-10 17:41:16 -0500

    "Update javaScript first paragraph function"

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,12 @@
 "use strict"; // Java script page
 
 // set up the IIFE (Pronounced 'IFFY')(Immediately Invoked Function Expression)
-(function(){
+(function () {
 
 	console.log("App has Started...");
-//anything inside the braces is called "Code Block" 
+	//anything inside the braces is called "Code Block" 
 
-// declared a named function way
+	// declared a named function way
 	/*function replaceFirstParagraph() {
 		console.log("inside replaceFirstParagraph function");
 		var firstParagraph;
@@ -16,16 +16,16 @@
 		firstParagraph.innerHTML = "My New Paragraph Data";
 }
 */
-// declare anonymous function with named alias
-	var replaceFirstParagraph = function() {
-	console.log("inside replaceFirstParagraph anonymous function");
+	// declare anonymous function with named alias
+	var replaceFirstParagraph = function () {
+		console.log("inside replaceFirstParagraph anonymous function");
 		var firstParagraph;
 
 		firstParagraph = document.getElementById("firstParagraph");
 
-		firstParagraph.innerHTML = "Welcome to my portfolio!";
-};
-// call the replaceFirstParagraph function
+		firstParagraph.innerHTML = "Hello! My name is Tony Crawford and I am a First Year Student in the Georgian College Interactive Web Development Program";
+	};
+	// call the replaceFirstParagraph function
 	replaceFirstParagraph();
 
 })();
